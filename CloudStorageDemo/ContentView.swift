1892c4081e1239a7e83f265e6370b65aea84bf13
FaiChou <faichou.zh@gmail.com>
2023-08-13 19:21:03 +0800

    demo 1

--- CloudStorageDemo/ContentView.swift
+++ CloudStorageDemo/ContentView.swift
@@ -8,19 +8,63 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var text: String = ""
+    @State private var showAlert1: Bool = false
+    @State private var showAlert2: Bool = false
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            TextField("Value", text: $text)
+                .padding()
+            Button("save") {
+                save()
+            }
+            .padding()
+            Button("restore") {
+                restore()
+            }
+            .padding()
+            Button("resotre2") {
+                if let x = NSUbiquitousKeyValueStore.default.string(forKey: "nonoooo") {
+                    print(x)
+                } else {
+                    print("novalue")
+                }
+            }
+        }
+        .alert(
+            "Save Failed",
+            isPresented: $showAlert1
+        ) {
+            Button("OK") {
+                // Handle the retry action.
+            }
+        }
+        .alert(
+            "Save Success",
+            isPresented: $showAlert2
+        ) {
+            Button("OK") {
+                // Handle the retry action.
+            }
         }
         .padding()
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    private func save() {
+        if text.isEmpty {
+            showAlert1 = true
+            return
+        }
+        let store = NSUbiquitousKeyValueStore.default
+        store.set(text, forKey: "k1")
+        store.synchronize()
+        showAlert2 = true
+    }
+    private func restore() {
+        let store = NSUbiquitousKeyValueStore.default
+        if let value = store.string(forKey: "k1") {
+            text = value
+        } else {
+            print("nothing to restore")
+        }
     }
 }
