e37ac58f79c9ace2ecbb3391c5fc7c0c28d4aebf
FaiChou <faichou.zh@gmail.com>
2023-08-13 15:51:31 +0800

    Initial Commit

---
+++ CloudStorageDemo/CloudStorageDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloudStorageDemoApp.swift
+//  CloudStorageDemo
+//
+//  Created by 周辉 on 2023/8/13.
+//
+
+import SwiftUI
+
+@main
+struct CloudStorageDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
